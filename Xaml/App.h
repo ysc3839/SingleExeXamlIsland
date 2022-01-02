aa7a71fd9b22457bf37d2886ee5d8d1cb82ac202
Richard Yu <yurichard3839@gmail.com>
2022-01-03 07:37:24 +0800

    Create new project

---
+++ Xaml/App.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "App.xaml.g.h"
+
+namespace winrt::Xaml::implementation
+{
+    struct App : AppT<App>
+    {
+        App();
+
+        void OnLaunched(Windows::ApplicationModel::Activation::LaunchActivatedEventArgs const&);
+        void OnSuspending(IInspectable const&, Windows::ApplicationModel::SuspendingEventArgs const&);
+        void OnNavigationFailed(IInspectable const&, Windows::UI::Xaml::Navigation::NavigationFailedEventArgs const&);
+    };
+}
